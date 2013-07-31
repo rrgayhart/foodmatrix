cadcca467aa087607aee64e86a4ee89cd3e360ac
Meeka Gayhart <rrgayhart@gmail.com>
2013-07-31 15:34:50 -0400

    Further changes to marinades

--- app/controllers/marinades_controller.rb
+++ app/controllers/marinades_controller.rb
@@ -1,2 +1,11 @@
 class MarinadesController < ApplicationController
+  def create
+    @spice = Spice.find(params[:spice_id])
+    @marinade = @spice.marinades.create(params[:marinade].permit(:name, :body, :time))
+    redirect_to spice_path(@spice)
+  end
+
+  	def index
+  		@marinades = Marinade.all
+  	end
 end
