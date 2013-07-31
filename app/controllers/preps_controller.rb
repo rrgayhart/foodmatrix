189c5ceb32ee3fe813d016f0341faef067da6c01
Meeka Gayhart <rrgayhart@gmail.com>
2013-07-31 10:32:21 -0400

    destroy function added

--- app/controllers/preps_controller.rb
+++ app/controllers/preps_controller.rb
@@ -4,4 +4,11 @@ class PrepsController < ApplicationController
     	@prep = @protein.preps.create(params[:prep].permit(:name, :body))
     	redirect_to protein_path(@protein)
   	end
+
+  	def destroy
+    	@protein = Protein.find(params[:protein_id])
+    	@prep = @protein.preps.find(params[:id])
+    	@prep.destroy
+    	redirect_to protein_path(@protein)
+  	end
 end
