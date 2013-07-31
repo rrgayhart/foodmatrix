c5248c247790e2c62b59cdad249088aedf97c02e
Meeka Gayhart <rrgayhart@gmail.com>
2013-07-31 07:08:17 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodmatrix::Application.config.secret_key_base = '0ac2a347a4174f50ab7c5042774f5defd8e61a7ba4a049580bba57b6734454b3d3d3a302e0d91109668d6031614d4d4d78e2a4f5f1f368c74f34e8f21e426a8c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Foodmatrix::Application.config.session_store :cookie_store, key: '_foodmatrix_session'
