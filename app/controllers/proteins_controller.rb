81fe181945b3a899dd157d586e67dc4bbdc665ad
Meeka Gayhart <rrgayhart@gmail.com>
2013-07-31 07:31:56 -0400

    adds detail to create in protein controller

--- app/controllers/proteins_controller.rb
+++ app/controllers/proteins_controller.rb
@@ -3,5 +3,14 @@ class ProteinsController < ApplicationController
 	end
 
 	def create
+		@protein = Protein.new(protein_params)
+
+		@protein.save
+		redirect_to @protein
 	end
+
+	private
+		def protein_params
+			params.require(:protein).permit(:title, :text)
+		end
 end
