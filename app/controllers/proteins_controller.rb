d65a3dfe44ef02a032fc7f6e24c7d4f2d8391c94
Meeka Gayhart <rrgayhart@gmail.com>
2013-07-31 09:21:53 -0400

    show and edit links added to proteins

--- app/controllers/proteins_controller.rb
+++ app/controllers/proteins_controller.rb
@@ -21,6 +21,20 @@ class ProteinsController < ApplicationController
 		@proteins = Protein.all
 	end
 
+	def edit
+		@protein = Protein.find(params[:id])
+	end
+
+	def update
+		@protein = Protein.find(params[:id])
+
+		if @protein.update(params[:protein].permit(:title, :text))
+			redirect_to @protein
+		else
+			render 'edit'
+		end
+	end
+	
 	private
 		def protein_params
 			params.require(:protein).permit(:title, :text)
