ab0beb6cf374710728cd8d4a7de1c97c9570789c
Meeka Gayhart <rrgayhart@gmail.com>
2013-08-01 16:18:55 -0400

    snacks additions and application css background

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -2,4 +2,21 @@ class SnacksController < ApplicationController
 	def index
 		@snacks = Snack.all
 	end
+
+	def new
+		@snacks = Snack.new
+	end
+
+	def show
+		@snack = Snack.find(params[:id])
+	end
+
+	def update
+		@snack = Snack.find(params[:id])
+		if @snack.update(params[:snack])
+			redirect_to(@book)
+		else
+			render :edit
+		end
+	end
 end
