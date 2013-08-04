83182e3d166a56b091e4de9f7aa619b3da1aedf7
Meeka Gayhart <rrgayhart@gmail.com>
2013-08-03 21:07:09 -0400

    Fixes 5 character validation on proteins

--- app/models/protein.rb
+++ app/models/protein.rb
@@ -1,5 +1,5 @@
 #Validation here is optional - checks to make sure title is long enough
 class Protein < ActiveRecord::Base
 	has_many :preps, dependent: :destroy
-	validates :title, presence: true, length: { minimum: 5 }
+	validates :title, presence: true
 end
