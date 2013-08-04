fbe132fb55b954f4a913b3c9bc1c415bbd38e740
Meeka Gayhart <rrgayhart@gmail.com>
2013-08-03 20:58:44 -0400

    Adds preps recipes and marinades to the meals and makes some corrections

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -6,6 +6,9 @@ class MealsController < ApplicationController
 		@protein = Protein.order("random()").first
 		@vegetable = Vegetable.order("random()").first
 		@spice = Spice.order("random()").first
+		@prep = @protein.preps.order("random()").first
+		@recipe = @vegetable.recipes.order("random()").first
+		@marinade = @spice.marinades.order("random()").first
 	end
 
 
--- app/models/spice.rb
+++ app/models/spice.rb
@@ -1,2 +1,3 @@
 class Spice < ActiveRecord::Base
+	has_many :marinades, dependent: :destroy
 end
