9c10112645d2d370e71287a30e8e9fbba378db5d
Meeka Gayhart <rrgayhart@gmail.com>
2013-08-07 15:03:27 -0400

    fixes random generate of meals bug

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,15 +1,8 @@
 class MealsController < ApplicationController
 	def index
+	  @meal = Meal.new
 	end
 	
-	def initialize
-		@protein = Protein.order("random()").first
-		@vegetable = Vegetable.order("random()").first
-		@spice = Spice.order("random()").first
-		@prep = @protein.preps.order("random()").first
-		@recipe = @vegetable.recipes.order("random()").first
-		@marinade = @spice.marinades.order("random()").first
-	end
-
+	private
 
 end
--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,3 +1,12 @@
 class Meal
 	attr_accessor :protein, :marinade, :vegetable, :spice, :fat, :recipe, :prep
+
+	def initialize
+		@protein = Protein.order("random()").first
+		@vegetable = Vegetable.order("random()").first
+		@spice = Spice.order("random()").first
+		@prep = @protein.preps.order("random()").first
+		@recipe = @vegetable.recipes.order("random()").first
+		@marinade = @spice.marinades.order("random()").first
+	end
 end
