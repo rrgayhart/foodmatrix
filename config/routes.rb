bf1c9f9864dc18ae956383f3036b1103038d4a2d
Meeka Gayhart <rrgayhart@gmail.com>
2013-08-09 14:15:50 -0400

    Adds sign in and sign out sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Foodmatrix::Application.routes.draw do
   
+  root 'welcome#index'
   resources :users
-  
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signup', to: 'users#new', via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
+  
   get "/meals" => "meals#index"
 
   resources :snacks
@@ -23,6 +27,6 @@ Foodmatrix::Application.routes.draw do
   	resources :preps
   end
  
-  root 'welcome#index'
+  
 end
 
