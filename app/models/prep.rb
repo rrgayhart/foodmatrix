5ab2dff470c8d2dc2314c04ec4a49b769619b746
Meeka Gayhart <rrgayhart@gmail.com>
2013-08-10 18:01:57 -0400

    clean up of prep display with changes to schema

--- app/models/prep.rb
+++ app/models/prep.rb
@@ -1,6 +1,6 @@
 class Prep < ActiveRecord::Base
 	has_many :protein_preps 
 	has_many :proteins, through: :protein_preps
- #has_many :prep_ingredients, dependent: :destroy
-  #has_many :ingredients, through: :prep_ingredients
+ 	has_many :prep_ingredients, dependent: :destroy
+  	has_many :ingredients, through: :prep_ingredients
 end
