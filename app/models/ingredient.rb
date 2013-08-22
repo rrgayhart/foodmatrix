e210a6680bd4126bc4df36d369cb77d612bd1ce9
Meeka Gayhart <rrgayhart@gmail.com>
2013-08-22 13:15:53 -0400

    Connect grocery_list to users and ingredients

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,4 +3,5 @@ class Ingredient < ActiveRecord::Base
   	has_many :recipes, through: :recipe_ingredients #Must remove after remove recipe
   	has_many :prep_ingredients
   	has_many :preps, through: :prep_ingredients
+  	has_many :grocery_lists
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
 		uniqueness: { case_sensitive: false }
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+	has_one :grocery_list
 	
 
 	def User.new_remember_token
